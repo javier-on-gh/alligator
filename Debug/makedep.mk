b0379964f21467b67d9486893cc62e9b49fab170
A01706702 <65085262+A01706702@users.noreply.github.com>
2024-06-21 13:30:08 -0600

    Add files via upload

---
+++ Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+bg95_mqtt.c
+
+DrvADC.c
+
+DrvSYS.c
+
+DrvTWI.c
+
+DrvUSART.c
+
+DrvWDT.c
+
+error_handling.c
+
+main.c
+
+MXC4005XC.c
+
+state_machine.c
+
